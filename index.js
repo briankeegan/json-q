9638ff31c7f9524acc8f29d62dd7e1fb878e7c40
briankeegan <brian.morris.keegan@gmail.com>
2018-05-15 12:06:17 -0400

    Complete versions 2
    Now can take path input as well as url
    And much more...

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+require('es6-promise').polyfill()
 const { loadCLI } = require('./lib/interface')
-const { exit } = require('./lib/messages')
-const { getData } = require('./lib/fetch')
+const { getData } = require('./lib/process')
+const { loadCommands } = require('./lib/commands')
 
-getData()
+loadCommands()
+
+getData(process.argv[2])
   .then(loadCLI)
-  .then(exit)
