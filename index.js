56b247365546c84f6a51d55ba092dd33722df9e7
briankeegan <brian.morris.keegan@gmail.com>
2018-05-16 11:18:34 -0400

    Add comments

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { loadCLI } = require('./lib/interface')
 const { getData } = require('./lib/process')
 const { loadCommands } = require('./lib/commands')
 
+// commands such as ctrl-l and ctrl-c
 loadCommands()
 
+// Pass in user paramter if it exists
 getData(process.argv[2])
+// Pass JS object to interface
   .then(loadCLI)
